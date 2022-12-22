d123c6b1cb28e1c12ff7e98f2eb9472f550038d5
Rohit Kushwah <rohitkushwah1742000@gmail.com>
2022-12-22 21:36:58 +0530

    Update timer_service.dart

--- lib/timer_service.dart
+++ lib/timer_service.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class TimerService extends ChangeNotifier{
-
+class TimerService extends ChangeNotifier {
+  late Timer timer;
+  double currentDuration = 1500;
+  double selectedTime = 1500;
 }
